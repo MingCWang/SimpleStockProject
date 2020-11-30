2a0c5263e318cbcb2b964f8f265ed0ba72220263
MING <TheSky@ming-shihs-mbp.lan>
2020-11-30 20:01:19 +0800

    final changes

--- SwingTrading/Class/PL.py
+++ SwingTrading/Class/PL.py
@@ -2,35 +2,33 @@ transactionfee = 0.001425
 discount = 0.0028
 tax = 0.003
 
-
-class Percentage():
-    def __init__(self, high, low, my):
+class ProfitLoss():
+    def __init__(self, high, low, my, units):
+        self.units = units
         self.high = high
         self.low = low
         self.my = my
 
-    def profit(self):
-        result = (self.high - self.my) / self.my
-        return result*100
-
-    def loss(self):
-        result = (self.low - self.my) / self.my
-        return result * 100
-
-
-class ProfitLoss(Percentage):
-    def __init__(self, high, low, my, units):
-        super().__init__(high, low, my)
-        self.units = units
+    def Buy(self):
+        cost = self.my * transactionfee * discount
+        Buy = self.my + cost
+        return Buy * self.units
 
     def Loss(self):
-        convert = self.units * 1000 # Convert stock price to actual money
         cost = (self.my * transactionfee * discount) + (self.low * transactionfee * discount) + (self.low * tax)
         loss = self.low - self.my - cost
-        return loss * convert
+        return loss * self.units
 
     def Profit(self):
-        convert = self.units * 1000
         cost = (self.my * transactionfee * discount) + (self.high * transactionfee * discount) + (self.high * tax)
         profit = self.high - self.my - cost
-        return profit * convert
+        return profit * self.units
+
+    def profitper(self):
+        result = self.Profit() / (self.my * self.units)
+        return result * 100
+
+    def lossper(self):
+        result = self.Loss() / (self.my * self.units)
+        return result * 100
+
--- SwingTrading/getvalue.py
+++ SwingTrading/getvalue.py
@@ -1,122 +1,116 @@
 import sys
 import os
 from datetime import datetime
-from Class.PL import Percentage, ProfitLoss 
+from Class.PL import ProfitLoss 
+
 today = datetime.today()
 currentdate = today.strftime('%Y-%m-%d %H:%M:%S')
 
-print()
-print('Risk Mangement'.upper(), ('6282.tw'))
-print()
-
-price = [] # Show defined variables
+# Show defined variables
+price = [] 
 lowprice = []
 upprice = []
 units = []
 date = []
 id = []
 choice = []
+stock = []
+Taiwan = '.tw'
 
+# Two fuctions deletedata() and savedata()
+def deletedata():
+    print()
+    choice = input('THIS WILL DELETE ALL DATAS ENTER [YES] TO CONFIRM ELSE ENTER [NO]: ')
+    choice = choice.upper()
+    print()
+    if choice == 'YES':
+        try:
+            os.path.exists('/Users/TheSky/Desktop/Python/SimpleStockProject/mydata.txt')
+            os.remove('/Users/TheSky/Desktop/Python/SimpleStockProject/mydata.txt')
+            print('[mydata.txt] file deleted \n')
+        except:
+            print('No existing datas yet! \n')
+    else:
+        print()
 
-
-
-def savedata(): #Save data to 'mydata.txt'
-    f = open('mydata.txt', 'a')
-    #f.write('SWING TRADING HISTORY \n')
+#Save data to 'mydata.txt'
+def savedata(): 
+    f = open('mydata.txt', 'a+')
     for i in range (0, len(price)):
-        per = Percentage(upprice[i], lowprice[i], price[i])
         PL = ProfitLoss(upprice[i], lowprice[i], price[i], units[i])
-        joinedtuple = (price[i], lowprice[i], upprice[i], units[i])
+        currentH = upprice[i] * units[i]
+        currentL = lowprice[i] * units[i]
         id.append(i)
         date.append(currentdate)
         f.write('\n')
         f.write('=============================== {} =================================='.format(date[i]))
         f.write('\n')
-        f.write('Buy, Low, High, Units')
+        f.write('{}{}'.format(stock, Taiwan))
         f.write('\n')
-        f.write(str(joinedtuple))
+        f.write('You bought %d (TWD)' % PL.Buy())
         f.write('\n\n')
-        f.write('Profit Percentage = %.3f %% \n' % (per.profit()))
-        f.write('Loss Percentage = %.3f %% \n' % (per.loss()))
+        f.write('If sold at {} (TWD)\n'.format(currentH))
+        f.write('Profit Percentage = %.3f %% \n' % (PL.profitper()))
         f.write('Profit [Selled stocks at high] = %.f \n' % (PL.Profit()))
+        f.write('\n')
+        f.write('If sold at {} (TWD)\n'.format(currentL))
+        f.write('Loss Percentage = %.3f %% \n' % (PL.lossper()))
         f.write('Loss [Selled stocks at low] = %.f \n' % (PL.Loss()))
         f.write('======================================================================================')
         f.write('\n')
     f.close()
+    print()
     print('DATA SAVED TO "mydata.txt"')
+    print()
 
+print()
+print('Simple Stock Profit Calculations')
+print()
 
-def printdata():
-    print('SWING TRADING HISTORY \n')
-    for i in range(0, len(price)):
-        per = Percentage(upprice[i], lowprice[i], price[i])
-        PL = ProfitLoss(upprice[i], lowprice[i], price[i], units[i])
-        joinedtuple = (price[i], lowprice[i], upprice[i], units[i])
-        date.append(currentdate)
-        id.append(i)
-        print('\n')
-        print('=============================== {} =================================='.format(id[i] + 1))
-        print('Datetime: ', date[i], '\n')
-        print('Buy, Low, High, Units')
-        print(joinedtuple)
-        print('\nProfit Percentage = %.3f %% \n' % (per.profit()))
-        print('Loss Percentage = %.3f %% \n' % (per.loss()))
-        print('Profit [Selled stocks at high] = %.f \n' % (PL.Profit()))
-        print('Loss [Selled stocks at low] = %.f' % (PL.Loss()))
-        print('======================================================================================')
-
+# main code below
 while True:
-    decision = input('[B] BUY STOCKS, [O] OPEN PURCHASE HISTORY, [S] SAVE DATA, [E] END, [D] DELETE DATA: ')
+    decision = input('[B] BUY STOCKS, [E] END, [D] DELETE DATA: ')
     print()
     decision = decision.upper()
     if decision == 'B':
+        decision2 = input('[O] ODD-LOT ORDERS (below 1000 shares), [N] NORMAL (1 = 1000 shares): ')
+        print()
+        decision2 = decision2.upper()
+        stock = input('Enter the company number: ')
+        print()
         while True:
             myprice = input('Insert buying price: ')
             price.append(float(myprice))
             print()
-            Units = (input('Insert buying units: '))
-            units.append(int(float(Units)))
+            if decision2 == 'O':
+                Units = input('Insert buying units: ')
+                units.append(int(float(Units)))
+            elif decision2 == 'N':
+                Units = int(float(input('Insert buying units (1 = 1000 shares): ')))
+                Units *= 1000
+                units.append(Units)
+            else: 
+                print('[error] try again\n')
+                break
             print()
             lastlowestprice = (input('Insert the last lowest price: '))
             lowprice.append(float(lastlowestprice))
             print()
-            lasthighestprice = (input('Insert the last highest price:'))
+            lasthighestprice = (input('Insert the last highest price: '))
             print()
             upprice.append(float(lasthighestprice))
             break
-
-    elif decision == 'O':
-        printdata()
-        flag = 0
-        while (flag == 0):
-            decision2 = input('[C] CONTINUE, [E] END: ')
-            decision2 = decision2.upper()
-            if decision2 == 'C':
-                print()
-                flag += 1
-                continue
-            elif decision2 == 'E':
-                sys.exit("Ended system")
-            else:
-                print('[error] try again')
-                continue 
-    elif decision == 'S':
-        savedata()
-        print()
     elif decision == 'E':
-        sys.exit("Ended system")
-        print()
+        decision3 = input('[S] SAVE DATA, [D] DELETE DATA: ')
+        decision3 = decision3.upper()
+        if decision3 == 'S':
+            savedata()
+            sys.exit("Ended system")
+        elif decision3 == 'D':
+            deletedata()
+            sys.exit("Ended system")
     elif decision == 'D':
-        choice = input('THIS WILL DELETE ALL DATAS ENTER [YES] TO CONFIRM ELSE ENTER [NO]: ')
-        choice = choice.upper()
-        if choice == 'YES':
-            os.remove('/Users/TheSky/Desktop/Python/SimpleStockProject/mydata.txt')
-            print('[mydata.txt] file deleted \n')
-        else:
-            print()
-            continue
+        deletedata()
     else: 
         print('[error] try again')
-        continue
-
- 
+        print()
